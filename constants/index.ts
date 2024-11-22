d78d252367dd920557fc5ed4b7cc8a61b5611207
Bitar Youssef <bitardev@gmail.com>
2024-11-22 08:57:20 +0100

    Add Assets and add provider for nextui

---
+++ constants/index.ts
@@ -0,0 +1,44 @@
+export const projetStepData = [
+  {
+    image: "/images/renovation/icon_devis.svg",
+    title: "Devis gratuit et sans engagement",
+    desc: "Profitez de notre service d'audit gratuit et recevez un devis détaillé pour vos travaux.",
+  },
+  {
+    image: "/images/renovation/icon_visite.svg",
+    title: "Visite technique de votre bien",
+    desc: "Un de nos experts effectuera une visite technique de votre bien pour évaluer vos besoins.",
+  },
+  {
+    image: "/images/renovation/icon_aides.svg",
+    title: "Aides détectées de votre devis",
+    desc: "Nous identifierons toutes les aides disponibles pour vous aider à financer vos travaux.",
+  },
+  {
+    image: "/images/renovation/icon_travaux.svg",
+    title: "Réalisation des travaux",
+    desc: "Nos équipes qualifiées réaliseront vos travaux en respectant les délais et la qualité.",
+  },
+];
+export const consultingService = [
+  {
+    image: "/images/renovation/rien_a_avancer.webp",
+    title: "Aucun Paiement Anticipé",
+    desc: "Chez Safeway Engineering, c'est simple ! Vous n'avez rien à avancer, quel que soit votre type de travaux. Payez seulement lorsque les travaux sont complétés à votre satisfaction.",
+  },
+  {
+    image: "/images/renovation/deplacement_gratuit.webp",
+    title: "Déplacement et Diagnostic Gratuit",
+    desc: "Nous intervenons là où vous le souhaitez, partout en France. Un expert évaluera vos besoins et vous proposera la meilleure solution pour optimiser votre rénovation, sans frais supplémentaires.",
+  },
+  {
+    image: "/images/renovation/aide_financement.webp",
+    title: "Aide au Financement",
+    desc: "Nous vous expliquons et vous aidons à accéder aux différentes possibilités de financement des travaux, telles que MaPrimeRénov', la prime CEE et d'autres aides locales. Réduisez significativement votre investissement initial grâce à notre expertise.",
+  },
+  {
+    image: "/images/renovation/specialiste.webp",
+    title: "Spécialistes Qualifiés",
+    desc: "Les travaux sont réalisés par des installateurs certifiés RGE, avec une double supervision pour garantir la qualité et respecter les délais. Faites confiance à nos professionnels pour une rénovation sans souci.",
+  },
+];
---
+++ types/index.d.ts
@@ -0,0 +1,3 @@
+declare interface childrenProps {
+  children: React.ReactNode;
+}
